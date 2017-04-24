e73b01cac0cdda3558bfe984c2ed0afd768aa5a5
Olfi01 <flom@meyer-alpers.de>
2017-04-24 18:21:23 +0200

    Added achievement bot to panel.

--- BotControlPanel/MainWindow.xaml.cs
+++ BotControlPanel/MainWindow.xaml.cs
@@ -28,11 +28,14 @@ namespace BotControlPanel
         private static readonly Brush inactiveBackground = (Brush)Application.Current.Resources["inactiveBackground"];
         private static readonly Brush erroredBackground = (Brush)Application.Current.Resources["erroredBackground"];
         private const string wwtbTokenPath = "C:\\Olfi01\\BotControlPanel\\.Tokens\\wwtb.token";
+        private const string achvTokenPath = "C:\\Olfi01\\BotControlPanel\\.Tokens\\achvbot.token";
         private const string tokenBasePath = "C:\\Olfi01\\BotControlPanel\\.Tokens\\";
         #endregion
         #region Variables
         private WWTB wwtb;
+        private WerewolfAchievementsBot achBot;
         private string wwtbToken = "";
+        private string achToken = "";
         #endregion
         #region Constructor
         public MainWindow()
@@ -42,6 +45,10 @@ namespace BotControlPanel
             try { wwtb = new WWTB(wwtbToken); }
 #pragma warning disable CS0168 // Variable ist deklariert, wird jedoch niemals verwendet
             catch (ArgumentException e) { textBlockWWTB.Background = erroredBackground; }
+#pragma warning restore CS0168 // Variable ist deklariert, wird jedoch niemals verwendet
+            try { achBot = new WerewolfAchievementsBot(achToken); }
+#pragma warning disable CS0168 // Variable ist deklariert, wird jedoch niemals verwendet
+            catch (ArgumentException e) { textBlockAchv.Background = erroredBackground; }
 #pragma warning restore CS0168 // Variable ist deklariert, wird jedoch niemals verwendet
         }
         #endregion
@@ -54,6 +61,12 @@ namespace BotControlPanel
             if (File.Exists(wwtbTokenPath))
             {
                 wwtbToken = File.ReadAllText(wwtbTokenPath);
+            }
+            #endregion
+            #region AchBot Token
+            if (File.Exists(achvTokenPath))
+            {
+                achToken = File.ReadAllText(achvTokenPath);
             }
             #endregion
         }
@@ -72,6 +85,22 @@ namespace BotControlPanel
             try { wwtb = new WWTB(wwtbToken); }
 #pragma warning disable CS0168 // Variable ist deklariert, wird jedoch niemals verwendet
             catch (ArgumentException e) { textBlockWWTB.Background = erroredBackground; }
+#pragma warning restore CS0168 // Variable ist deklariert, wird jedoch niemals verwendet
+        }
+        #endregion
+        #region AchBot Token
+        private void setAchvToken(string token)
+        {
+            if (!Directory.Exists(tokenBasePath))
+            {
+                Directory.CreateDirectory(tokenBasePath);
+            }
+            File.WriteAllText(achvTokenPath, token);
+            achToken = token;
+            textBlockAchv.Background = inactiveBackground;
+            try { achBot = new WerewolfAchievementsBot(achToken); }
+#pragma warning disable CS0168 // Variable ist deklariert, wird jedoch niemals verwendet
+            catch (ArgumentException e) { textBlockAchv.Background = erroredBackground; }
 #pragma warning restore CS0168 // Variable ist deklariert, wird jedoch niemals verwendet
         }
         #endregion
@@ -116,6 +145,46 @@ namespace BotControlPanel
             TokenDialog td = new TokenDialog(wwtbToken);
             td.ShowDialog();
             setWWTBToken(td.result);
+        }
+        #endregion
+
+        #region Start AchBot
+        private void buttonStartAchievements_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBlockAchv.Background == erroredBackground)
+            {
+                MessageBox.Show("Lege zuerst ein korrektes Token unter Einstellungen fest!");
+                return;
+            }
+            if (!achBot.isRunning)
+            {
+                achBot.startBot();
+            }
+            textBlockAchv.Background = activeBackground;
+            buttonStartAchievements.Content = "Stoppen";
+            buttonStartAchievements.Click -= buttonStartAchievements_Click;
+            buttonStartAchievements.Click += buttonStopAchievements_Click;
+        }
+        #endregion
+        #region Stop AchBot
+        private void buttonStopAchievements_Click(object sender, RoutedEventArgs e)
+        {
+            if (achBot.isRunning)
+            {
+                achBot.stopBot();
+            }
+            textBlockAchv.Background = inactiveBackground;
+            buttonStartAchievements.Content = "Starten";
+            buttonStartAchievements.Click -= buttonStopAchievements_Click;
+            buttonStartAchievements.Click += buttonStartAchievements_Click;
+        }
+        #endregion
+        #region Achievement Settings
+        private void buttonSettingsAchievements_Click(object sender, RoutedEventArgs e)
+        {
+            TokenDialog td = new TokenDialog(achToken);
+            td.ShowDialog();
+            setAchvToken(td.result);
         }
         #endregion
         #endregion
