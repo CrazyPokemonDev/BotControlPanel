4fdf06982db886d481ef6b9dd2372ebc0431b2fe
Olfi01 <flom@meyer-alpers.de>
2017-04-24 18:32:17 +0200

    Added Achievements Bot fuctionality, except for Client_OnUpdate

--- BotControlPanel/Bots/WerewolfAchievementsBot.cs
+++ BotControlPanel/Bots/WerewolfAchievementsBot.cs
@@ -3,11 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot;
 
 namespace BotControlPanel.Bots
 {
     public class WerewolfAchievementsBot
     {
+        #region Variables
+        private TelegramBotClient client;
+        #endregion
+        public WerewolfAchievementsBot(string token)
+        {
+            client = new TelegramBotClient(token);
+            client.OnUpdate += Client_OnUpdate;
+        }
 
+        private void Client_OnUpdate(object sender, Telegram.Bot.Args.UpdateEventArgs e)
+        {
+            
+        }
+
+        #region Control Methods
+        #region Is running
+        public bool isRunning { get { return client.IsReceiving; } }
+        #endregion
+        #region Start Bot
+        public bool startBot()
+        {
+            if (!client.IsReceiving)
+            {
+                client.StartReceiving();
+                return true;
+            }
+            return true;
+        }
+        #endregion
+        #region Stop Bot
+        public bool stopBot()
+        {
+            if (client.IsReceiving)
+            {
+                client.StopReceiving();
+                return true;
+            }
+            return true;
+        }
+        #endregion
+        #endregion
     }
 }
