bf4e640dd823a24e5a9b521c0143f4c7663426c3
Olgabrezel <lumpes8524@gmail.com>
2017-04-24 22:00:30 +0200

    Started coding....
    .....and started wondering if what I code is a good idea.... maybe it
    makes things way too complicated O.o. Whatever.

--- BotControlPanel/Bots/WerewolfAchievementsBot.cs
+++ BotControlPanel/Bots/WerewolfAchievementsBot.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot;
+using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 
@@ -12,10 +13,67 @@ namespace BotControlPanel.Bots
 {
     public class WerewolfAchievementsBot
     {
+        class Game
+        {
+            public Message pinmessage { get; set; }
+            public List<User> players { get; set; }
+            public state gamestate { get; set; }
+            private TelegramBotClient client;
+
+
+            private const string joinMessageText = "*Join this game!*\n\nPin this message and remember to press start as soon as the game starts.";
+            private string playerlist;
+
+            public Game(TelegramBotClient cl)
+            {
+                client = cl;
+                UpdatePlayerlist();
+            }
+
+            public enum state
+            {
+                Joining,
+                Running
+            }
+
+            public bool AddPlayer(User newplayer)
+            {
+                if (!players.Contains(newplayer) && gamestate == state.Joining)
+                {
+                    players.Add(newplayer);
+                    UpdatePlayerlist();
+                    return true;                    
+                }
+                return false;
+            }
+
+
+            public bool RemovePlayer(User oldplayer)
+            {
+                if(players.Contains(oldplayer))
+                {
+                    players.Remove(oldplayer);
+                    UpdatePlayerlist();
+                    return true;
+                }
+                return false;
+            }
+
+            private void UpdatePlayerlist()
+            {
+                playerlist = "*Players:*";
+
+                foreach(var p in players)
+                {
+                    playerlist += "\n" + p.FirstName;
+                }
+                client.EditMessageTextAsync(pinmessage.Chat.Id, pinmessage.MessageId, joinMessageText + "\n\n" + playerlist, parseMode: ParseMode.Markdown);
+            }
+        }
+
         #region Variables
         private TelegramBotClient client;
-        private Dictionary<long, int> groupsMessageIdsDict = new Dictionary<long, int>();
-        private Dictionary<long, string> groupsMessagesDict = new Dictionary<long, string>();
+        private Dictionary<long, Game> games = new Dictionary<long, Game>();
         #endregion
         #region Constructor
         public WerewolfAchievementsBot(string token)
@@ -32,37 +90,76 @@ namespace BotControlPanel.Bots
             {
                 if (e.Update.Message.Type == MessageType.TextMessage)
                 {
-                    #region Stargame recognized
-                    if((e.Update.Message.Text == "/startgame@werewolfbot" ||
-                        e.Update.Message.Text == "/startgame" ||
-                        e.Update.Message.Text == "/startgame@werewolfbetabot" ||
-                        e.Update.Message.Text == "/startchaos@werewolfbot" ||
-                        e.Update.Message.Text == "/startchaos" ||
-                        e.Update.Message.Text == "/startchaos@werewolfbetabot")
-                        && (e.Update.Message.Chat.Type == ChatType.Group ||
-                        e.Update.Message.Chat.Type == ChatType.Supergroup))
+                    var text = e.Update.Message.Text;
+                    var msg = e.Update.Message;
+
+                    switch(text.Replace("@werewolfbot", "").Replace('!', '/').Replace("@werewolfachievementbot", ""))
                     {
-                        IReplyMarkup rm = InlineKeyboardTellRole.Get("werewolfwolfachievementbot");
-                        Task t = client.SendTextMessageAsync(e.Update.Message.Chat.Id,
-                            "➡️*CURRENT GAME*⬅️\nNo roles given yet",
-                            parseMode: ParseMode.Markdown,
-                            replyMarkup: rm);
-                        if (groupsMessageIdsDict.ContainsKey(e.Update.Message.Chat.Id))
-                        {
-                            groupsMessageIdsDict.Remove(e.Update.Message.Chat.Id);
-                        }
-                        groupsMessageIdsDict.Add(e.Update.Message.Chat.Id, e.Update.Message.MessageId);
-                        if (groupsMessagesDict.ContainsKey(e.Update.Message.Chat.Id))
-                        {
-                            groupsMessagesDict.Remove(e.Update.Message.Chat.Id);
-                        }
-                        groupsMessagesDict.Add(e.Update.Message.Chat.Id, "➡️*CURRENT GAME*⬅️");
+                        case "/startgame":
+                        case "/startchaos":
+                            if (games.ContainsKey(msg.Chat.Id))
+                            {
+                                client.SendTextMessageAsync(msg.Chat.Id, "It seems there is already a game running in here! Stop that before you start a new one!");
+                            }
+                            else
+                            {
+                                var gamemessage = client.SendTextMessageAsync(msg.Chat.Id, "Initializing new game...").Result;
+                                var gameplayers = new List<User>();
+                                var game = new Game(client)
+                                {
+                                    players = gameplayers,
+                                    gamestate = Game.state.Joining,
+                                    pinmessage = gamemessage
+                                };
+
+                                games.Add(msg.Chat.Id, game);
+
+                                games[msg.Chat.Id].AddPlayer(msg.From);
+                            }
+                            break;
+
+                        case "/join":
+                            if(games.ContainsKey(msg.Chat.Id) && games[msg.Chat.Id].gamestate == Game.state.Joining)
+                            {
+                                if (!games[msg.Chat.Id].AddPlayer(msg.From))
+                                {
+                                    client.SendTextMessageAsync(msg.Chat.Id, "Failed to add " + msg.From.FirstName + " to the players!");
+                                }
+                            }
+                            else
+                            {
+                                client.SendTextMessageAsync(msg.Chat.Id, "It seems there is no game running in your group, or it can't be joined at the moment.");
+                            }
+                            break;
+
+                        case "/flee":
+                            if(games.ContainsKey(msg.Chat.Id) && games[msg.Chat.Id].gamestate == Game.state.Joining)
+                            {
+                                if(!games[msg.Chat.Id].RemovePlayer(msg.From))
+                                {
+                                    client.SendTextMessageAsync(msg.Chat.Id, "Failed to remove " + msg.From.FirstName + " from the players!");
+                                }
+                            }
+                            else
+                            {
+                                client.SendTextMessageAsync(msg.Chat.Id, "It seems there is no game running in your group, or it can't be joined at the moment.");
+                            }
+                            break;
+
+                        case "/dead":
+                            if(games.ContainsKey(msg.Chat.Id) && games[msg.Chat.Id].gamestate == Game.state.Running)
+                            {
+                                if(!games[msg.Chat.Id].RemovePlayer(msg.From))
+                                {
+                                    client.SendTextMessageAsync(msg.Chat.Id, "Failed to remove dead player " + msg.From.FirstName + "...");
+                                }
+                            }
+                            else
+                            {
+                                client.SendTextMessageAsync(msg.Chat.Id, "It seems there is no running game at the moment, so no player can be dead!");
+                            }
+                            break;
                     }
-                    #endregion
-
-                    #region Custom start
-
-                    #endregion
                 }
             }
         }
