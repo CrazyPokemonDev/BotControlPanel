233a7bb94f701dbbbf337ff72651a7b383641642
Olfi01 <flom@meyer-alpers.de>
2017-05-23 22:03:32 +0200

    GLOBAL SUPPRESSIONS HALLELUJAH

---
+++ BotControlPanel/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Stil", "IDE0018:Inlinevariablendeklaration", Justification = "<Ausstehend>", Scope = "member", Target = "~M:BotControlPanel.Bots.ScriptingBot.Client_OnUpdate(System.Object,Telegram.Bot.Args.UpdateEventArgs)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Stil", "IDE0018:Inlinevariablendeklaration", Justification = "<Ausstehend>", Scope = "member", Target = "~M:BotControlPanel.Bots.Wwtb.HandleTextMessage(Telegram.Bot.Types.Message)")]
+
