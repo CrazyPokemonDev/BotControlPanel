2175cd5f9e84470c2c1f2d37bdf43e5a77466300
Olgabrezel <lumpes8524@gmail.com>
2017-05-23 20:23:59 +0200

    Don't announce maint mode in test group repeatedly

--- BotControlPanel/Bots/WerewolfAchievementsBotPlus.cs
+++ BotControlPanel/Bots/WerewolfAchievementsBotPlus.cs
@@ -647,7 +647,7 @@ namespace BotControlPanel.Bots
                                 "The game is now considered stopped.").Wait();
                             client.SendTextMessageAsync(id, $"<b>{e.CallbackQuery.From.FirstName}</b> has considered the game stopped!", parseMode: ParseMode.Html);
                             justCalledStop.Remove(e.CallbackQuery.From.Id);
-                            if (maint)
+                            if (maint && id != allowedgroups[0])
                             {
                                 client.SendTextMessageAsync(id, "<b>The bot is in maintenance mode, so you can't start any further games for now!</b>", parseMode: ParseMode.Html);
                                 client.SendTextMessageAsync(allowedgroups[0], $"1 Game just finished. There are {games.Count} more games running.");
@@ -811,7 +811,7 @@ namespace BotControlPanel.Bots
                                             games.Remove(msg.Chat.Id);
                                             ReplyToMessage($"<b>{msg.From.FirstName}</b> has considered the game stopped!", u);
                                             justCalledStop.Remove(msg.From.Id);
-                                            if (maint)
+                                            if (maint && msg.Chat.Id != allowedgroups[0])
                                             {
                                                 client.SendTextMessageAsync(msg.Chat.Id, "<b>The bot is in maintenance mode, so you can't start any further games for now!</b>", parseMode: ParseMode.Html);
                                                 client.SendTextMessageAsync(allowedgroups[0], $"1 Game just finished. There are {games.Count} more games running.");
